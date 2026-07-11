60508774a863ff57cb9cc3d88b87d6b949ba9c4f
Bosi Chen <chenbosi.bosi@gmail.com>
2026-07-10 23:01:57 -0400

    test github actions workflow 2

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,10 @@
 # Define the resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.label_prefix}-A09-rg"
-  location = var.region
+  name="${var.label_prefix}-A09-rg" location=var.region
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                = ${var.label_prefix}storageacc
+  name                = "${var.label_prefix}storageacc"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
