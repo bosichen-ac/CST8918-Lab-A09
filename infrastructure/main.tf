a1f545fa64a92fdb2a299752925344531e84b5d8
Bosi Chen <chenbosi.bosi@gmail.com>
2026-07-10 22:39:51 -0400

    fix tf error

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,9 +5,9 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = ${var.label_prefix}storageacc
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  name                = "${var.label_prefix}storageacc"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 
   account_tier             = "Standard"
   account_replication_type = "LRS"
